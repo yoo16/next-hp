1a9b2fb5fc9ccd59aa9e0481ed0bdbf704afec99
yoo16 <yohei.yoshikawa@gmail.com>
2025-07-16 10:54:36 +0900

    update

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import { Product } from '@/types/Product';
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 interface Props {
     product: Product;
@@ -7,12 +10,19 @@ interface Props {
 
 export default function ProductCard({ product }: Props) {
     return (
-        <div className="relative flex flex-col items-center p-4">
+        <motion.div
+            className="relative flex flex-col items-center p-4"
+            whileHover={{
+                scale: 1.03,
+                transition: { type: 'spring', stiffness: 300, damping: 20 },
+            }}
+        >
             {product.recommend && (
                 <div className="absolute top-2 right-2 bg-red-500 text-white text-sm px-4 py-1 rounded-full shadow">
                     {product.recommend}
                 </div>
             )}
+
             <div className="rounded-3xl overflow-hidden">
                 <Image
                     src={product.image}
@@ -22,9 +32,10 @@ export default function ProductCard({ product }: Props) {
                     className="rounded shadow-md"
                 />
             </div>
+
             <div className="w-64">
                 <p className="mt-4 text-lg font-bold">{product.name}</p>
             </div>
-        </div>
+        </motion.div>
     );
 }
